ade62c38f58a5387d1655c040b07a360760f4348
Lana Lisovs'ka <lana@lisovskii.com>
2024-11-14 17:20:25 +0200

    resolve warm Loader

--- components/Loader.js
+++ components/Loader.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Loader() {
   return (
     <div className="flex justify-center items-center h-[400px]">
-      <Image alt="Loading..." src="/loader.gif" width={200} height={200} />
+      <Image  priority={true} alt="Loading..." src="/loader.gif" width={200} height={200} />
     </div>
   );
 }
